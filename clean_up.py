427285b1c0c41f501bbe346084d6084f6c542a16
neeraj1bh <neeraj1bh@gmail.com>
2020-03-06 19:03:44 +0530

    Updated the file

--- clean_up.py
+++ clean_up.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 Extensions = {
 "Images": [".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", "svg", 
            ".psd"], 
@@ -7,10 +10,22 @@ Extensions = {
               ".rtf", ".rtfd", ".xls", ".xlsx", ".ppt", "pptx"], 
 "Archives": [".iso", ".tar", ".gz", ".7z", ".rar",".zip"], 
 "Audio": [".aac", ".m4a", ".mp3", "ogg", ".raw", ".wav", ".wma"], 
-"Text": [".txt", ".in", ".out"], 
+"Text": [".txt", ".in", ".out"]
 }
 
 # {'.jpeg': 'Images', '.jpg': 'Images',...., '.mp4': 'Videos'}
 data = {file_ext: folder
             for folder, file_formats in Extensions.items()
                 for file_ext in file_formats}
+
+
+if __name__ == "__main__":
+    print()
+    print("How to Find What We Organised:")
+    print("Audio Folder        - Contains Audio files.")
+    print("Videos Folder       - Contains Video files.")
+    print("Images Folder       - Contains Image files.")
+    print("Archives Folder     - Contains Compressed files and Other Archives.")
+    print("Documents Folder    - Contains Documents files from Libre Office / MS Office.")
+    print("Text Folder         - Contains Plain Text files.")
+    input('Hit enter to exit')                
